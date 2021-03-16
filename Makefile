93dbbd3d68eb97a48534ab1b26595f10677ebca9
Kevin <skryfall@gmail.com>
2021-03-15 20:44:24 -0600

    [UPDATE] Now it shows a pixel on screen

--- Makefile
+++ Makefile
@@ -1,22 +1,21 @@
 qemu:
-	nasm -fbin src/boot.asm -o src/boot.bin
-	nasm -fbin src/helloworld.asm -o src/helloworld.bin
-	cat src/boot.bin src/helloworld.bin > out/program.bin
+	nasm -fbin src/boot.asm -o out/program.bin
 	qemu-system-i386 out/program.bin
 
 debugServer:
-	nasm -fbin src/boot.asm -o src/boot.bin
-	nasm -fbin src/helloworld.asm -o src/helloworld.bin
-	cat src/boot.bin src/helloworld.bin > out/program.bin
+	nasm -fbin src/boot.asm -o out/program.bin
 	qemu-system-i386 -s -S out/program.bin
 
 debugClient:
 	gdb -ex "target remote localhost:1234" -ex "set architecture i8086" -ex "set disassembly-flavor intel" -ex "b *0x7C00" -ex "b *0x8000" -ex "c"
 
 hardware:
-	nasm -fbin src/boot.asm -o src/boot.bin
-	nasm -fbin src/helloworld.asm -o src/helloworld.bin
-	cat src/boot.bin src/helloworld.bin > out/program.bin
+	nasm -fbin src/boot.asm -o out/program.bin
+	sudo umount /dev/sdb1
+	sudo dd if=out/program.bin of=/dev/sdb1 bs=512 count=1
+
+hardwareU:
+	nasm -fbin src/boot.asm -o src/program.bin
 	sudo dd if=out/program.bin of=/dev/sdb1 bs=512 count=1
 
 clean:
--- src/boot.asm
+++ src/boot.asm
@@ -1,30 +1,38 @@
-org 0x7C00
+org 0x7c00
+bits 16
 
-;Initialize Registers
-cli
-xor ax, ax
-mov ds, ax
-mov ss, ax
-mov es, ax
-mov fs, ax
-mov gs, ax
-mov sp, 0x6ef0 ; setup the stack like qemu does
-sti
+    jmp short start    ; Salta al inicio
 
-;Reset disk system
-mov ah, 0
-int 0x13 ; 0x13 ah=0 dl = drive number
+alienx  dw  0ah        ; Pos x del alien
+alieny  dw  0ah        ; Pos y del alien
 
-;Read from harddrive and write to RAM
-mov bx, 0x8000     ; bx = address to write the kernel to
-mov al, 1 		   ; al = amount of sectors to read
-mov ch, 0          ; cylinder/track = 0
-mov dh, 0          ; head           = 0
-mov cl, 2          ; sector         = 2
-mov ah, 2          ; ah = 2: read from drive
-int 0x13   		   ; => ah = status, al = amount read
-jmp 0x8000
-times 510-($-$$) db 0
-;Begin MBR Signature
-db 0x55 ;byte 511 = 0x55
-db 0xAA ;byte 512 = 0xAA
+start:
+    call    initializeBootloader
+    call    initializeDisplay
+    call    drawPixel
+    jmp     $
+
+initializeBootloader:  ; Requerido para inicializar el bootloader
+    xor     ax, ax
+    mov     ds, ax
+    mov     es, ax
+
+    ret
+
+initializeDisplay:
+    mov     ah, 00h    ; Activar el modo video
+    mov     al, 13h    ; 320x200 256 colores
+    int     10h        ; Ejecutar interrupcion
+    ret                ; Retornar
+
+drawPixel:
+    mov     ah, 0ch    ; Dibuja pixel
+    mov     al, 02h    ; Color blanco
+    mov     bh, 00h    ; Pagina
+    mov     cx, [alienx] ; Pos x
+    mov     dx, [alieny] ; Pos y
+    int     10h        ; Ejecutar interrupcion
+    ret                ; Retornar
+
+times   510-($-$$) db 0
+dw      0xaa55
